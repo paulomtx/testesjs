33e48e1114ab1507de5566e9acd758021a5b6a5d
PAULO LUIZ MELO TEXEIRA <PAULO0381@DIGITALCOLLEGE.LOCAL>
2024-07-04 16:21:56 -0300

    atividade_sala

---
+++ calculoetiqueta.js
@@ -0,0 +1,7 @@
+let valor = document.getElementById('valor')
+let qtd =document.querySelector('#quantidade')
+let corEtiquetas= document.getElementsByName('cor')
+
+function calcularvalorfinal(){
+
+}
