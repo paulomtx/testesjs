141807b9a7e85c5ffe5f4e479590e0b79d8aac40
PAULO LUIZ MELO TEXEIRA <PAULO0381@DIGITALCOLLEGE.LOCAL>
2024-07-05 15:34:31 -0300

    atividadeonclick

---
+++ quadrocores.js
@@ -0,0 +1,11 @@
+let btnvermelho=document.getElementsByTagName('buttom')[0]
+let btnverde=document.getElementsByTagName('buttom')[1]
+let btnazul =document.getElementsByTagName('buttom')[2]
+
+
+let quadro =document.querySelector('.quadro')
+
+function mudarcor(cor){
+    quadro.style.background=cor
+
+}
